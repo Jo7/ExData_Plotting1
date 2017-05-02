dd4f92eea3ac3efc1f7a3af34c25498705aa9be4
Jodie Reed <jodie8112@yahoo.co.uk>
2017-05-02 13:32:47 +0200

    Commit from RStudio

--- plot4.R
+++ plot4.R
@@ -16,17 +16,16 @@ tail(dat)
 #Combine date and time and convert to date time classes
 dat$DateTime <- strptime(paste(dat$Date, dat$Time, sep = ""), "%d/%m/%Y %H:%M:%S")
 
-#Plot 3 and save as file device
+#Plot 4 and save as file device
+png("plot4.png", width=480, height=480)
 par(mfrow = c(2,2))
 par(mar = c(4,4,2,2))
 plot(dat$DateTime, dat$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
-plot(dat$DateTime, dat$Voltage, type = "l", ylab = "Voltage", xlab = "")
+plot(dat$DateTime, dat$Voltage, type = "l", xlab = "datetime",ylab = "Voltage")
 plot(dat$DateTime, dat$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(dat$DateTime, dat$Sub_metering_2, col = "red")
 lines(dat$DateTime, dat$Sub_metering_3, col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        lty = 1, lwd = 1, col = c("black", "red", "blue"))
-plot(dat$DateTime, dat$Global_reactive_power, type = "l", xlab = "", ylab = "Global Reactive Power (kilowatts)")
-
-dev.copy(png, file = "plot4.png" ,width = 480, height = 480)  
+plot(dat$DateTime, dat$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power (kilowatts)")
 dev.off()
